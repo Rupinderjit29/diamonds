c6b8797dbdd927c08515f52dff2672864070fee4
Rupinderjit29 <106723128+Rupinderjit29@users.noreply.github.com>
2022-11-14 16:01:57 -0500

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -9,7 +9,13 @@ import pickle
 import numpy as np
 import streamlit as st
 #loaded_model=pickle.load(open('C:/Users/Iqbal/Desktop/web/trained_model.sav','rb')
-loaded_model=pickle.load(open('trained_model.sav','rb'))                        
+loaded_model=pickle.load(open('trained_model.sav','rb'))      
+
+inputdata=(0.23,4,5,1,61.5,55,3.95,3.98,2.43)
+inputdata=np.asarray(inputdata)
+inputdata=inputdata.reshape(1,-1)
+a=loaded_model.predict(inputdata)
+
 
 def main():   
                          
@@ -18,7 +24,7 @@ def main():
     
     if st.button('Predict Price'):
        # price = predict1(carat, cut, color, clarity, depth, table, x, y, z)
-      st.success(f'The predicted price of the diamond is USD')
+      st.success(f'The predicted price of the diamond is USD $a')
         
    
 if __name__=='__main__':
