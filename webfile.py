207561506c73e4483a1d6665f5eb4a175c482fa1
Rupinderjit29 <106723128+Rupinderjit29@users.noreply.github.com>
2022-11-14 11:22:00 -0500

    Update webfile.py

--- webfile.py
+++ webfile.py
@@ -9,7 +9,7 @@ import pickle
 import numpy as np
 import streamlit as st
 import pandas as pd
-loaded_model=pickle.load(open('https://github.com/Rupinderjit29/diamonds/blob/main/trained_model.sav','rb'))
+loaded_model=pickle.load(open('https://github.com/Rupinderjit29/diamonds/blob/47bd4a7bd3b714e84b6bd98a41e0b081fd96d82e/trained_model.sav','rb'))
 
 
 def predict(carat, cut, color, clarity, depth, table, x, y, z):
