47bd4a7bd3b714e84b6bd98a41e0b081fd96d82e
Rupinderjit29 <106723128+Rupinderjit29@users.noreply.github.com>
2022-11-14 11:01:11 -0500

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 13 23:43:09 2022
+
+@author: Iqbal
+"""
+
+import pickle
+import numpy as np
+import streamlit as st
+#loaded_model=pickle.load(open('C:/Users/Iqbal/Desktop/web/trained_model.sav','rb')
+                         
+
+def main():   
+                         
+    st.title('Diamond Price Predictor')
+    st.header('Enter the characteristics of the diamond:')
+   
+if __name__=='__main__':
+    main()                        
