e939e37493f311fd51ab9bce67c6d75f98079b92
Rupinderjit29 <106723128+Rupinderjit29@users.noreply.github.com>
2022-11-14 15:52:36 -0500

    Update untitled1.py

--- untitled1.py
+++ untitled1.py
@@ -9,7 +9,7 @@ import pickle
 import numpy as np
 import streamlit as st
 #loaded_model=pickle.load(open('C:/Users/Iqbal/Desktop/web/trained_model.sav','rb')
-                         
+loaded_model=pickle.load(open('trained_model.sav','rb'))                        
 
 def main():   
                          
