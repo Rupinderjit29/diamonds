02c3071fe2b73639df0c75fef3a6554988b948e5
Rupinderjit29 <106723128+Rupinderjit29@users.noreply.github.com>
2022-11-14 14:02:20 -0500

    Update webfile.py

--- webfile.py
+++ webfile.py
@@ -9,7 +9,7 @@ import pickle
 import numpy as np
 import streamlit as st
 import pandas as pd
-import xgboost as xbg
+import xgboost as xgb
 model=xgb.XGBRegressor
 model.load_model('trained_model.sav')
 #loaded_model=pickle.load(open('https://github.com/Rupinderjit29/diamonds/blob/47bd4a7bd3b714e84b6bd98a41e0b081fd96d82e/trained_model.sav','rb'))
